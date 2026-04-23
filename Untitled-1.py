1cad568321e6d3805b24fe8ab11ceaf16782b26a
etergames <86607734+etergames@users.noreply.github.com>
2026-04-22 21:12:50 -0300

    probamos otra vez

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,3 +2,8 @@ print("esto es un test, no se que poner aqui soy nami hola");
 if(1 == 1){
     print("AGUS SE LA COME ENTERA");
 }
+i = 0;
+while(i < 10){
+    print("AGUS SE LA COME ENTERA");
+    i = i + 1;
+}   
